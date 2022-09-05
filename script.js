b9d2d39ebadc610873ad81014187f05e7a61d5df
sanjay <72781686+sanjay-kandpal@users.noreply.github.com>
2022-09-05 13:53:12 +0530

    update1.1

---
+++ script.js
@@ -0,0 +1,45 @@
+var words = ['Hello! I am front-end developer in search of internship.'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('#about-para').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
+
+// Hello! I am front-end developer in search of work to improve my skills
